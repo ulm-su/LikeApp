008fa2190854df7e719cd38788d4b127ec80f73c
Алексей Онин <a@ulm.su>
2020-03-26 14:21:18 +0300

    Добавлена реализация классов ClientLog, Log.

---
+++ app/src/main/java/su/ulm/android/lib/ClientLog.java
@@ -0,0 +1,12 @@
+package su.ulm.android.lib;
+
+public class ClientLog
+{
+  public static void setOut (String name)
+  {
+  }
+
+  public static void setErr (String name)
+  {
+  }
+}
---
+++ app/src/main/java/su/ulm/android/lib/Log.java
@@ -0,0 +1,20 @@
+package su.ulm.android.lib;
+
+public final class Log
+{
+  public static void setCheckLoggable (boolean s)
+  {
+  }
+
+  public static void i (String tag, String s)
+  {
+  }
+
+  public static void w (String tag, String s)
+  {
+  }
+
+  public static void e (String tag, String s, Exception e)
+  {
+  }
+}
